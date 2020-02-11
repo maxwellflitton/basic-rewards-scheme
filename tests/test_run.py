009d00606a3ebb7104f6df072ef5cfee590bd6fd
Archives <archives@archivess-mbp.mynet>
2020-02-11 23:42:49 +0000

    tests done

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,15 +1,29 @@
 from unittest import TestCase, main
-from mock import patch
+from mock import patch, MagicMock
 
-from run import create_app
+from singleton import Singleton
 
 
 class TestCreateApp(TestCase):
 
-    @patch("run.Flask")
-    def test_create_app(self, mock_app):
-        test = create_app()
-        self.assertEqual(mock_app.return_value, test)
+    def tearDown(self) -> None:
+        Singleton._instances = {}
+
+    @patch("flask.Flask")
+    @patch("flask_sqlalchemy.SQLAlchemy")
+    def test___init__(self, mock_db, mock_app):
+        from run import AppEngine
+        test = AppEngine()
+        self.assertEqual(mock_app.return_value, test.app)
+        self.assertEqual(mock_db.return_value, test.db)
+
+    @patch("flask.Flask")
+    @patch("flask_sqlalchemy.SQLAlchemy")
+    @patch("models.user.User")
+    def test_import_models(self, mock_user, mock_db, mock_app):
+        from run import AppEngine
+        test = AppEngine()
+        test.db = MagicMock()
 
 
 if __name__ == "__main__":
