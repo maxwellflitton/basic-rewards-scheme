d57766b13dcd33265dd95fa3c9a8d0c9699794f0
Archives <archives@Archivess-MacBook-Pro.local>
2020-02-15 14:04:35 +0000

    credits field added

--- src/models/user.py
+++ src/models/user.py
@@ -12,6 +12,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     unique_id = db.Column(db.String(120), unique=True)
+    credit_amount = db.Column(db.Integer, default=0, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128))
