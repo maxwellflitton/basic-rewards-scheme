9f0215d6e1dafd3119c944cba482817b7862bde0
Archives <archives@Archivess-MacBook-Pro.local>
2020-02-15 18:52:00 +0000

    unit tests done

--- src/config.py
+++ src/config.py
@@ -5,14 +5,23 @@ from singleton import Singleton
 
 
 class GlobalParams(dict, metaclass=Singleton):
-
+    """
+    This is a class for managing the global params from a config yml file.
+    """
     def __init__(self):
+        """
+        The constructor for the GlobalParams class.
+        """
         super().__init__()
-        self.config_path = sys.argv[-1]
         self.update(self.get_yml_file())
 
     @staticmethod
-    def get_yml_file():
-        with open(sys.argv[1]) as file:
+    def get_yml_file() -> dict:
+        """
+        Opens a yaml file from sys.argv[-1].
+
+        :return: (dict) data from yaml file
+        """
+        with open(sys.argv[-1]) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
         return data
